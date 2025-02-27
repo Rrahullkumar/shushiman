b643d360c7211763a4222968c81a95423d77781b
Rrahullkumar <rahulkr811844@gmail.com>
2025-02-27 21:40:13 +0530

    adding deployed frontend url

--- Server-node/src/server.js
+++ Server-node/src/server.js
@@ -158,7 +158,7 @@ const startServer = async () => {
     if (!isProduction) {
       const PORT = process.env.PORT || 5000;
       app.listen(PORT, () => {
-        console.log(`🚀 Server running on port ${PORT}`);
+        console.log(` Server running on port ${PORT}`);
       });
     }
   } catch (err) {
